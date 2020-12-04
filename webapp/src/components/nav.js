3d347a95ec4d8257aea60451103270ee3780421d
Ishani Bose <bose.i@northeastern.edu>
2020-12-04 03:38:40 -0500

    Responsive Nav Bar changes

--- webapp/src/components/nav.js
+++ webapp/src/components/nav.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-import {
-  Nav,
-  Navbar,
-  NavDropdown,
-  NavItem
-} from "react-bootstrap";
+import { Nav, Navbar, NavDropdown, NavItem } from "react-bootstrap";
 import { MDBRow, MDBCol, MDBIcon } from "mdbreact";
 
 class Header extends Component {
   state = {};
   render() {
     return (
-      <Navbar expand="md" sticky="top" className="navbar navbar-custom">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        className="navbar navbar-custom"
+      >
         <img
           src="/assets/Logo.png"
           width="200"
@@ -19,20 +19,22 @@ class Header extends Component {
           className="d-inline-block align-middle"
           alt="React Bootstrap logo"
         />
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
-            <NavItem className="navbar-text">
-                Hi, User
-            </NavItem>
+            <NavItem className="navbar-text">Hi, User</NavItem>
             <Nav.Link href="#home">Logout</Nav.Link>
             <NavDropdown title="Select Login Option" id="basic-nav-dropdown">
-              <NavDropdown.Item>Login with Google</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item>Login with Email</NavDropdown.Item>
+                 <NavDropdown.Item>Login with Google</NavDropdown.Item>
+                 <NavDropdown.Divider />
+                 <NavDropdown.Item>Login with Email</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link>Help</Nav.Link>
-            <NavItem><MDBIcon icon="question-circle" /></NavItem>
+            <NavItem>
+                <MDBIcon icon="question-circle" />
+            </NavItem>
           </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
