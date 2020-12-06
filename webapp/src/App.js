74c630ec360494e64bc2ca168c83e3dc26b9e29c
Ishani Bose <bose.i@northeastern.edu>
2020-12-05 22:14:17 -0500

    Responsive Footer

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,21 @@
 import './dist/App.css';
 import React, { Component } from 'react';
 import Header from './components/nav';
+import Footer from './components/footer';
 
 
 class App extends Component {
   state = {  }
   render() { 
-    return ( <Header /> );
+    return ( 
+      <div className="page-container">
+        <div className="content-wrap">
+          <Header /> 
+          <Footer />
+        </div>
+      </div>
+   
+    );
   }
 }
  
